c680c082428ef6a6a08fe381e7d9aa6f7b0598f5
Bocanegra <juanjosebocanegra777@gmail.com>
2026-08-28 16:02:08 -0500

    feat: configuracion completa para pipeline ci/cd

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from sample_app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
